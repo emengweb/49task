da01b163aadd4d9bc37ec3056050d5bb7db109f7
emengweb <31469739+emengweb@users.noreply.github.com>
2021-03-16 15:06:12 +0800

    Update scheduler.js
    任务的随机延迟时间，最大120s

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -69,8 +69,8 @@ let scheduler = {
         for (let taskName of taskNames) {
             let options = tasks[taskName].options || {}
             let willTime = moment(randomDate(options));
-            // 任务的随机延迟时间
-            let waitTime = options.dev ? 0 : Math.floor(Math.random() * (options.waitTime || 300))
+            // 任务的随机延迟时间，最大120s
+            let waitTime = options.dev ? 0 : Math.floor(Math.random() * (options.waitTime || 120))
             if (options) {
                 if (options.isCircle || options.dev) {
                     willTime = moment().startOf('days');
