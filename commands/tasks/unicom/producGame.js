7b5c0b1c904496203d60db452ca0dbda1ffdb1e8
emengweb <31469739+emengweb@users.noreply.github.com>
2021-03-22 17:06:05 +0800

    allgames

--- commands/tasks/unicom/producGame.js
+++ commands/tasks/unicom/producGame.js
@@ -366,7 +366,7 @@ var producGame = {
         let { popularList: allgames, jar } = await producGame.popularGames(axios, options)
 //         let games = await producGame.timeTaskQuery(axios, options)
 //         games = allgames.filter(g => games.filter(g => g.state === '0').map(i => i.gameId).indexOf(g.id) !== -1)
-        games = allgames
+        let games = allgames
         console.info('剩余未完成game', games.length)
         let queue = new PQueue({ concurrency: 2 });
 
